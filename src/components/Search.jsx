2fed7d60f0bfef4e49545cef549891064387cb34
m-honarzade <honarzadeh.mahdie@gmail.com>
2023-10-10 12:16:31 +0330

    add useRef for focus in search box with load the page and pressing enter key

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 const Search = ({ query, setQuery }) => {
+  const inputEl = useRef(null);
+  useEffect(() => {
+    inputEl.current.focus();
+    const callBack = (e) => {
+      if (document.activeElement === inputEl.current) {
+        return;
+      }
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callBack);
+    return document.addEventListener("keydown", callBack);
+  }, [setQuery]);
+
   return (
     <input
       type="text"
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
       className="w-1/2 h-10 rounded-lg bg-[#7950f2] text-[#adb5bd] px-4 "
     />
   );
